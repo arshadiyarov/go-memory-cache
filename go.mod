0bd742f7fd73e8d72c46d40e1b4c1e3173227801
init-armat <armat@init.red>
2025-01-05 22:35:22 +0500

    initial commit for cache package

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/arshadiyarov/go-memory-cache
+
+go 1.23.4
---
+++ main.go
@@ -0,0 +1,27 @@
+package cache
+
+type Cache struct {
+	data map[string]interface{}
+}
+
+func New() *Cache {
+	return &Cache{
+		data: make(map[string]interface{}),
+	}
+}
+
+func (c *Cache) Set(k string, v interface{}) {
+	c.data[k] = v
+}
+
+func (c *Cache) Get(k string) interface{} {
+	return c.data[k]
+}
+
+func (c *Cache) Delete(k string) {
+	delete(c.data, k)
+}
+
+func (c *Cache) GetAll() map[string]interface{} {
+	return c.data
+}
